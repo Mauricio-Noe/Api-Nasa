2b5b0a7c460e19ecd873fa15a2ea772f347aef28
Noe <gnoe8799@gmail.com>
2024-11-07 00:08:38 -0600

    cambios de seguridad

--- src/componentes/context1.jsx
+++ src/componentes/context1.jsx
@@ -1,46 +1,4 @@
-/*import React, {createContext, useEffect, useState} from "react";
 
-export const Apinasa = createContext();
- 
-export const Apiprovaider = ({children}) => {
- 
-    const [data, setData] = useState (null);
-    const [loading, setLoading ] = useState ( true);
-    const [error, setError]= useState= (null);
-
-    const fetchData = async () =>{
-            setLoading(true);
-    try{
-        const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=YeZXHY29QdWZ8XkDeCoPAEh1wnTSv7LmXwxxIw09`);
-
-        if(!response.ok){
-        throw new Error("Error");
-        
-        }
-
-        const result = await response.json();
-        setData(result);
-    } catch(error){
-        setError(error.message);
-    }finally{
-    setLoading (false);
-    }
-
-        };
-
-        useEffect(() => {
-            fetchData();
-          }, []);
-        
-          return (
-            <Apinasa.Provider value={{ data, loading, error, fetchData }}>
-              {children}
-            </Apinasa.Provider>
-          );
-        
-
-};
-*/
 import React, { createContext, useState } from "react";
 
 export const Apinasa = createContext();
@@ -54,7 +12,9 @@ export const Apiprovaider = ({ children }) => {
     setLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=YeZXHY29QdWZ8XkDeCoPAEh1wnTSv7LmXwxxIw09");
+
+      const apikey= process.env.REACT_APP_NASA_API_KEY;
+      const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apikey}`);
       if (!response.ok) {
         throw new Error("Error en la solicitud");
       }
